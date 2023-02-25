7c223e3d4538fdf020643dfbcf814fef4c5f996f
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-02-26 02:52:45 +0300

    Create assign.c

---
+++ assign.c
@@ -0,0 +1,32 @@
+#include "shell.h"
+
+/**
+*assign - assign value to every member of ShellData
+*@shell_data: pointer to shell data variable type to fill
+*its memory
+*@command_line_args: vectors
+*Return: void
+*/
+
+void assign(ShellData *shell_data, char **command_line_args)
+{
+	unsigned int i;
+
+	shell_data->command_counter = 1;
+	shell_data->exit_status = 0;
+	shell_data->command_line_args = command_line_args;
+	shell_data->user_input = NULL;
+	shell_data->parsed_input_args = NULL;
+	shell_data->environment_vars = NULL;
+	shell_data->process_id = aux_itoa(getpid());
+
+	for (i = 0; environ[i]; i++)
+		;
+
+	shell_data->environment_vars = malloc(sizeof(char *) * (i + 1));
+
+	for (i = 0; environ[i]; i++)
+		shell_data->environment_vars[i] = strdup(environ[i]);
+
+	shell_data->environment_vars[i] = NULL;
+}
