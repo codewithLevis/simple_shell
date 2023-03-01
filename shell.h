b2202c3cc769ad079c167ea09dd364703a53c36c
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-01 21:12:32 +0300

    Update shell.h

--- shell.h
+++ shell.h
@@ -35,4 +35,59 @@ char *_itoa(int num);
 char *_strdup(char *str);
 char *_memcpy(char *dest, char *src, unsigned int n);
 
+/**
+*struct Builtin - structure for built-in commands
+*@name: name of the built-in command (e.g. cd, exit, env)
+*@func: function pointer to the corresponding built-in function
+*/
+typedef struct Builtin
+{
+	char *name;
+	int (*func)(data_shell *datash);
+} Builtin;
+
+/**
+*struct VarNode_t - represents a node in a singly linked list that stores variables
+*@len_var: length of the variable
+*@value: value of the variable
+*@len_val: length of the value
+*@next: pointer to the next node in the list
+*Description: This struct defines a node in a singly linked list used to store variables.
+*The len_var field holds the length of the variable, the val field holds the value of the variable,
+*the len_val field holds the length of the value, and the next field points to the next node in the list.
+*/
+typedef struct VarNode_t
+{
+	int len_var;
+	char *value;
+	int len_val;
+	struct VarNode_t *next;
+} VarNode_t;
+
+/**
+*struct SeparatorNode - single linked list
+*@symbol: ; | &
+*@next: pointer to next node
+*Description: single linked list to store separators
+*stores able to separate the commands entered by the user
+*into different parts to be executed separately
+*/
+typedef struct SeparatorNode
+{
+	char symbol;
+	struct SeparatorNode *next;
+} VarNode_t;
+
+/**
+*struct CommandLineNode - singly linked list for command lines
+*@command: the command line string
+*@next: pointer to the next node in the list
+*A singly linked list structure used to store command lines.
+*/
+typedef struct CommandLineNode
+{
+	char *command;
+	struct CommandLineNode *next;
+} CommandLineNode;
+
 #endif /*SHELL_H*/
