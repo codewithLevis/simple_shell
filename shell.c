fdc07d857a196233cb13f76a7074b7cc4fde402d
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-02 11:31:03 +0300

    Update shell.c

--- shell.c
+++ shell.c
@@ -15,5 +15,8 @@ int main(__attribute__((unused))int argc, char *argv[])
 	signal(SIGINT, sigint_handler);
 	assign(ptr_data, argv);
 	program_integration(ptr_data);
-	return (0);
+	free_shell_data(ptr_data);
+	if ((*ptr_data).exit_status < 0)
+		return (255);
+	return ((*ptr_data).exit_status);
 }
