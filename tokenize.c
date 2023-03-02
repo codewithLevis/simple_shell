3640d2ca8366649c838479402efe9eb4567b7219
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-03 00:54:00 +0300

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -10,28 +10,28 @@
 
 char **my_reallocate(char **ptr_arr, unsigned int old_len, unsigned int new_len)
 {
-	char **new;
+	char **new_arr;
 	unsigned int i;
 
 	if (ptr_arr == NULL)
 		return (malloc(sizeof(char*) * new_len));
 
 	if (new_len == old_len)
-		return (pointer_array);
+		return (ptr_arr);
 
-	new = malloc(sizeof(char*) * new_len);
+	new_arr = malloc(sizeof(char*) * new_len);
 
-	if (new_pointer_array == NULL)
+	if (new_arr == NULL)
 		return (NULL);
 
 	if (new_len < old_len)
 		old_len = new_len;
 
 	for (i = 0; i < old_len; i++)
-		new[i] = ptr_arr[i];
+		new_arr[i] = ptr_arr[i];
 
 	free(ptr_arr);
-	return (new);
+	return (new_arr);
 }
 
 /**
@@ -45,7 +45,7 @@ char **tokenize(char *input, const char *delim)
 {
 	size_t bufsize = TOK_BUFSIZE;
 	size_t i;
-	char token,**tokens = malloc(bufsize * sizeof(char*));
+	char *token,**tokens = malloc(bufsize * sizeof(char*));
 
 	if (tokens == NULL)
 		return (NULL);
@@ -55,7 +55,7 @@ char **tokenize(char *input, const char *delim)
 	i = 0;
 	while (token != NULL)
 	{
-		tokens[i++] = strdup(token);
+		tokens[i++] = _strdup(token);
 		if (i >= bufsize)
 		{
 			bufsize += TOK_BUFSIZE;
