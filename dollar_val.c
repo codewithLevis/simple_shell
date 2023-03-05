93f3ac7d9775704fb4231390ca6f19bb1b43c899
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-05 17:10:25 +0300

    Update dollar_val.c

--- dollar_val.c
+++ dollar_val.c
@@ -57,40 +57,32 @@ void search_env_var(VarNode_t **start, char *in, ShellData *ptr)
 int parse_vars(VarNode_t **head, char *input, char *status, ShellData *ptr)
 {
 	int i, index = 0, status_len = 0, pid_len = 0;
+
 	status_len = _strlen(status);
 	pid_len = _strlen(ptr->process_id);
-	
+
 	for (i = 0; input[i]; i++)
 	{
-		switch (input[i])
+		if (input[i] == '$')
 		{
-			case '$':
-				if (input[i + 1] == '?')
-				{
-					add_node(head, 2, status, status_len);
-					i++;
-				}
-				else if (input[i + 1] == '$')
-				{
-					add_node(head, 2, ptr->process_id, pid_len);
-					i++;
-				}
-				else if (input[i + 1] == '\n' || input[i + 1] == '\0')
-					add_node(head, 0, NULL, 0);
-				else if (input[i + 1] == ' ' || input[i + 1] == '\t')
-					add_node(head, 0, NULL, 0);
-				else if (input[i + 1] == ';')
-					add_node(head, 0, NULL, 0);
-				else
-					search_env_var(head, input + i, ptr);
-				break;
-			default:
-				break;
+			if (input[i + 1] == '?')
+				add_node(head, 2, status, status_len);
+
+			else if (input[i + 1] == '$')
+				add_node(head, 2, ptr->process_id, pid_len);
+
+			else if (input[i + 1] == '\n' || input[i + 1] == '\0')
+				add_node(head, 0, NULL, 0);
+
+			else if (input[i + 1] == ' ' || input[i + 1] == '\t' || input[i + 1] == ';')
+				add_node(head, 0, NULL, 0);
+			else
+				search_env_var(head, input + i, ptr);
 		}
-		index++;
 	}
-	return (index);
+	return (i);
 }
+
 /**
 *expansion - function expands environment variables in the input string
 *@start: pointer to head of a linked list
