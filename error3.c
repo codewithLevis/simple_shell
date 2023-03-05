01d3d977f9a63e8c13bed41cc32eb7aec225f21b
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-05 20:34:10 +0300

    Update error3.c

--- error3.c
+++ error3.c
@@ -8,35 +8,36 @@
 
 char *generate_cd_err(ShellData *ptr)
 {
-        int len, len_id;
-        char *err, *str, *msg, *args, *arg_in;
+	int len, len_id;
+	char *err, *str, *msg, *args, *arg_in;
 
-        str = _itoa(ptr->command_counter);
-        if ( ptr->parsed_input_args[1][0] == '-')
-        {
-                msg = ": Illegal option ";
-                len_id = 2;
-        }
-        else
-        {
-                msg = ": can't cd to ";
-                len_id = _strlen( ptr->parsed_input_args[1]);
-        }
-        args = ptr->command_line_args[0];
-        arg_in = ptr->parsed_input_args[0];
-        len = _strlen(args) + _strlen(arg_in);
-        len += _strlen(str) + _strlen(msg) + len_id + 5;
-        err = malloc(sizeof(char) * (len + 1));
-        if (err == NULL)
-        {
-                free(str);
-                return (NULL);
-        }
+	str = _itoa(ptr->command_counter);
 
-        sprintf(err, "%s: %s: %s%s%s", args, str, msg, ptr->parsed_input_args[1], "\n");
+	if (ptr->parsed_input_args[1][0] == '-')
+	{
+		msg = ": Illegal option ";
+		len_id = 2;
+	}
+	else
+	{
+		msg = ": can't cd to ";
+		len_id = _strlen(ptr->parsed_input_args[1]);
+	}
+	args = ptr->command_line_args[0];
+	arg_in = ptr->parsed_input_args[0];
+	len = _strlen(args) + _strlen(arg_in);
+	len += _strlen(str) + _strlen(msg) + len_id + 5;
+	err = malloc(sizeof(char) * (len + 1));
+	if (err == NULL)
+	{
+		free(str);
+		return (NULL);
+	}
 
-        free(str);
-        return (err);
+	sprintf(err, "%s: %s: %s%s%s", args, str, msg, ptr->parsed_input_args[1], "\n");
+
+	free(str);
+	return (err);
 }
 
 /**
@@ -47,31 +48,31 @@ char *generate_cd_err(ShellData *ptr)
  */
 int search_cmd_error(char *dir, ShellData *ptr)
 {
-        if (dir == NULL)
-        {
-                handle_error(ptr, 127);
-                return (1);
-        }
+	if (dir == NULL)
+	{
+		handle_error(ptr, 127);
+		return (1);
+	}
 
-        if (_strcmp((*ptr).parsed_input_args[0], dir) != 0)
-        {
-                if (access(dir, X_OK) == -1)
-                {
-                        handle_error(ptr, 126);
-                        free(dir);
-                        return (1);
-                }
-                free(dir);
-        }
-        
-        else
-        {
-                if (access((*ptr).parsed_input_args[0], X_OK) == -1)
-                {
-                        handle_error(ptr, 126);
-                        return (1);
-                }
-        }
+	if (_strcmp((*ptr).parsed_input_args[0], dir) != 0)
+	{
+		if (access(dir, X_OK) == -1)
+		{
+			handle_error(ptr, 126);
+			free(dir);
+			return (1);
+		}
+		free(dir);
+	}
 
-        return (0);
+	else
+	{
+		if (access((*ptr).parsed_input_args[0], X_OK) == -1)
+		{
+			handle_error(ptr, 126);
+			return (1);
+		}
+	}
+
+	return (0);
 }
