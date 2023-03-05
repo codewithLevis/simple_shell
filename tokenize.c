43f60442a4908e4cda48ec151a395f30810182a4
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-05 21:54:11 +0300

    Update tokenize.c

--- tokenize.c
+++ tokenize.c
@@ -2,24 +2,24 @@
 
 /**
 *my_reallocate - allocate memory for array of strings
-*@ptr_arr: pointer to an array of strings
+*@ptr: pointer to an array of strings
 *@old_len: old array size
 *@new_len: new array size
 *Return: pointer to a newly allocated memory
 */
 
-char **my_reallocate(char **ptr_arr, unsigned int old_len, unsigned int new_len)
+char **my_reallocate(char **ptr, unsigned int old_len, unsigned int new_len)
 {
 	char **new_arr;
 	unsigned int i;
 
-	if (ptr_arr == NULL)
-		return (malloc(sizeof(char*) * new_len));
+	if (ptr == NULL)
+		return (malloc(sizeof(char *) * new_len));
 
 	if (new_len == old_len)
-		return (ptr_arr);
+		return (ptr);
 
-	new_arr = malloc(sizeof(char*) * new_len);
+	new_arr = malloc(sizeof(char *) * new_len);
 
 	if (new_arr == NULL)
 		return (NULL);
@@ -30,13 +30,13 @@ char **my_reallocate(char **ptr_arr, unsigned int old_len, unsigned int new_len)
 	for (i = 0; i < old_len; i++)
 		new_arr[i] = ptr_arr[i];
 
-	free(ptr_arr);
+	free(ptr);
 	return (new_arr);
 }
 
 /**
 *tokenize - splits input to tokens
-*@str: pointer to input
+*@input: pointer to input
 *@delim: delimiter
 *Return: an array of strings
 */
@@ -45,7 +45,7 @@ char **tokenize(char *input, const char *delim)
 {
 	size_t bufsize = TOK_BUFSIZE;
 	size_t i;
-	char *token,**tokens = malloc(bufsize * sizeof(char*));
+	char *token, **tokens = malloc(bufsize * sizeof(char *));
 
 	if (tokens == NULL)
 	{
@@ -62,7 +62,7 @@ char **tokenize(char *input, const char *delim)
 		if (i >= bufsize)
 		{
 			bufsize += TOK_BUFSIZE;
-			tokens = my_reallocate(tokens, i, bufsize * sizeof(char*));
+			tokens = my_reallocate(tokens, i, bufsize * sizeof(char *));
 
 			if (tokens == NULL)
 			{
