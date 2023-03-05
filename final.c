654d489d11b3e13b7eb4ff1b67732bfefad26dd8
codewithLevis <117777219+codewithLevis@users.noreply.github.com>
2023-03-05 21:06:23 +0300

    Update final.c

--- final.c
+++ final.c
@@ -11,7 +11,7 @@ int is_command_executable(char *command, ShellData *ptr)
 {
 	struct stat st;
 	int i, index = -1;
-	
+
 	for (i = 0; command[i]; i++)
 	{
 		if (command[i] == '.')
@@ -56,7 +56,8 @@ int is_command_executable(char *command, ShellData *ptr)
 int is_path_segment(char *path, int *index)
 {
 	if (path[*index] == ':')
-			return (1);
+		return (1);
+
 	for (; path[*index]; (*index)++)
 	{
 		if (path[*index] == ':')
@@ -117,56 +118,61 @@ char *get_path(char *cmd, char **envp)
 }
 
 /**
-*execute_cmd - to execute a shell command given its arguments, wait for the command to complete
+*execute_cmd - to execute a shell command given
+*its arguments, wait for the command to complete
 *and set the status of the data_shell struct accordingly
 *@arg: parsed input
 *@environ_vars: environment variables
 *@ptr: pointer to shell struct
 *Return: first checks if the command is executable and returns 1 if not
-*then, it checks if the command exists in the PATH environment variable and sets its path accordingly
+*then, it checks if the command exists
+*in the PATH environment variable and sets its path accordingly
 *If there is an error with the command, the function returns 1
-*the function forks a child process to execute the command using the execve system call
+*the function forks a child
+*process to execute the command using the execve system call
 */
 int execute_cmd(ShellData *shell_data)
 {
-    pid_t pid;
-    pid_t wait_pid;
-    int status;
-    int exec_flag;
-    char *directory;
+	pid_t pid;
+	pid_t wait_pid;
+	int status;
+	int exec_flag;
+	char *directory, *arg_in = shell_data->parsed_input_args[0];
+	char **envp = shell_data->environment_vars;
 
-    exec_flag = is_command_executable(shell_data->parsed_input_args[0], shell_data);
-    if (exec_flag == -1)
-        return (1);
-    if (exec_flag == 0)
-    {
-        directory = get_path(shell_data->parsed_input_args[0], shell_data->environment_vars);
-        if (search_cmd_error(directory, shell_data) == 1)
-            return (1);
-    }
-    pid = fork();
-    if (pid == 0)
-    {
-        if (exec_flag == 0)
-            directory = get_path(shell_data->parsed_input_args[0], shell_data->environment_vars);
-        else
-            directory = shell_data->parsed_input_args[0];
-        execve(directory + exec_flag, shell_data->parsed_input_args, shell_data->environment_vars);
-    }
-    else if (pid < 0)
-    {
-        perror(shell_data->command_line_args[0]);
-        return (1);
-    }
-    else
-    {
-        do {
-            wait_pid = waitpid(pid, &status, WUNTRACED);
-        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
-    }
+	exec_flag = is_command_executable(arg_in, shell_data);
+	if (exec_flag == -1)
+		return (1);
+	if (exec_flag == 0)
+	{
+		directory = get_path(arg_in, envp);
+		if (search_cmd_error(directory, shell_data) == 1)
+			return (1);
+	}
+	pid = fork();
 
-    shell_data->exit_status = status / 256;
-    return (1);
+	if (pid == 0)
+	{
+		if (exec_flag == 0)
+			directory = get_path(arg_in, envp);
+		else
+			directory = arg_in;
+		execve(directory + exec_flag, shell_data->parsed_input_args, envp);
+	}
+	else if (pid < 0)
+	{
+		perror(shell_data->command_line_args[0]);
+		return (1);
+	}
+	else
+	{
+		do {
+			wait_pid = waitpid(pid, &status, WUNTRACED);
+		} while (!WIFEXITED(status) && !WIFSIGNALED(status));
+	}
+
+	shell_data->exit_status = (status / 256);
+	return (1);
 }
 
 
